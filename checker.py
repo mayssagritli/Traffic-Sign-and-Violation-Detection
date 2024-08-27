35dc60c050f8d9df90175df8b4e533e6451fd99a
mayssagritli <mayssa.gritli@hotmail.fr>
2024-08-27 09:57:00 +0100

    v5

--- checker.py
+++ checker.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import cv2
 from datetime import datetime
-
+#class hedhi tekhou 5 frames t9ayadhom each time , l threshold howa 9adeh lezm l ratio mtaa l sign fl whole image yfout bch tthseb lkarhba taht lblaka 
 class CarControl:
     def __init__(self,fps,width,height, frames_to_check=5, threshold=0.01, db_config=None,rec_size=100):
         self.frames_to_check = frames_to_check
@@ -9,6 +9,7 @@ class CarControl:
         self.width = width
         self.height = height
         self.threshold = threshold
+#history fih l frames l 5 eli n9aydou fehom esm lclasse taa sign , l ratio eli lezm akber mel threshold w l confidence  
         self.history = []
         for i in range(frames_to_check):
             self.history.append(["none", 0, 0])
@@ -23,33 +24,34 @@ class CarControl:
         
         self.in_stop = False
         self.curr_speed_lim = 50
-        self.db_config = {
-    'host': 'sql7.freemysqlhosting.net',
-    'user': 'sql7720134',
-    'database': 'sql7720134',
-    'password': '3b6uDHWTa3',
-    'port' : '3306'
-}
-
-
-          # Database connection
+        #details l db eli bch n9aydou feha l violations 
+        #Database connection
+        self.db_config= {
+                            'host': 'sql7.freemysqlhosting.net',
+                            'user': 'sql7720134',
+                            'database': 'sql7720134',
+                            'password': '3b6uDHWTa3',
+                            'port' : '3306'
+                        }
         self.db_connection = None
         self.db_cursor = None
         self.connect_to_db()
       
         if self.db_config:
             self.connect_to_db()
+#log nhebou nchoufou y9ayed wale (just testing)            
     def log_viol(self, violID, type):
         time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         log_entry = f"{violID}_{type}_{time}\n"
         log_file = open("log.txt", "a")
         log_file.write(log_entry)
         log_file.close()       
+        
     def update_rec(self,frame):
         self.record.append(frame)
         if(len(self.record) > self.max_rec_size):
             self.record.pop(0)
-
+#db error and exception handling 
     def connect_to_db(self):
         try:
             self.db_connection = mysql.connector.connect(**self.db_config)
@@ -57,28 +59,28 @@ class CarControl:
             print("Successfully connected to the database")
         except mysql.connector.Error as err:
             print(f"Error connecting to MySQL database: {err}")
-
+#lhne naabiw l list taa l history chykoun feha list mtaa 5 frames kol frame aandou [calssname,ratio,confidence]
     def update(self, sign_class_name, ratio,conf):
         self.history.append([sign_class_name, ratio, conf])
         if len(self.history) > self.frames_to_check:
             self.history.pop(0)
-        
+#lhne check ken l sign red wale        
     def check_red(self,conf):
         for i in self.history:
             if i[0] == "Red Light" and i[1] >= self.threshold:
                 self.recent_red_conf =conf
                 self.in_red = True
-            
+ #check ken l sign green detected               
     def check_green(self):
         for i in self.history:
             if i[0] == "Green Light" and i[1] >= self.threshold:
                 self.in_red = False
-    
+#check ken l sign Stop        
     def check_stop(self):
         for i in self.history:
             if i[0] == "Stop" and i[1] >= self.threshold:
                 self.in_stop = True
-            
+ #check for which speed limit detected              
     def update_speed(self):
         speed_limit_class_names = ["Speed Limit 10", "Speed Limit 20", "Speed Limit 30", "Speed Limit 40",
                            "Speed Limit 50", "Speed Limit 60", "Speed Limit 70", "Speed Limit 80",
@@ -95,14 +97,13 @@ class CarControl:
             out.write(f)
     
         out.release()
-        
+#lhne lkhedma taa violations        
     def check_violation(self, speed):
         violation_type = None
         confidence_score = 0
 
         if self.in_stop and speed > 5:
             violation_type = "STOP VIOLATION"
-            
             confidence_score = max([i[2] for i in self.history if i[0] == "Stop"])
 
         if speed > self.curr_speed_lim:
@@ -110,21 +111,23 @@ class CarControl:
             confidence_score = 0.8
 
         a = 0
+        b = 0
         for i in self.history:
+            if  i[1] < self.threshold or i[0] != "Stop":
+                b += 1
             if  i[1] < self.threshold or i[0] != "Red Light":
                 a += 1
-        if a == self.frames_to_check:
+        if b == self.frames_to_check:
             self.in_stop = False
+        if a == self.frames_to_check:
             if self.in_red:
                 violation_type = "RED LIGHT VIOLATION"
-                
             self.in_red = False
 
         if violation_type:
             print(violation_type)
             if violation_type == "RED LIGHT VIOLATION":
                 type = 'RD'
-                
             elif violation_type == "SPEED LIMIT EXCEEDED":
                 type = "SP"
             else:
@@ -140,6 +143,7 @@ class CarControl:
             cursor.close()
             self.save_violation(newID, violation_type, speed, p, confidence_score)
             self.log_viol(newID, violation_type)
+  #lhne nsavi l results fl db           
     def save_violation(self, detection_id, violation_type, speed, video_path, confidence_score):
         if not self.db_connection:
             print("Database connection not established. Cannot save violation.")
